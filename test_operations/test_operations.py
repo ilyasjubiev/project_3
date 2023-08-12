66596f2c512d5e29d566666746bfdf5d18077b99
ilyas <jubiev2017@gmail.com>
2023-08-12 21:29:45 +0300

    Initial commit

--- test_operations/test_operations.py
+++ test_operations/test_operations.py
@@ -1,14 +1,42 @@
-import pytest
-
-from utils import unpacking_data
-from utils import get_date
+from utils.utils import get_date, unpacking_data, filtered_data, get_last_values, get_from_or_to, get_list_of_operations
 
 
-#def test_unpacking_data():
+def test_unpacking_data():
+    data = unpacking_data()
+    assert isinstance(data, list)
 
-    #data = unpacking_data()
-   # assert isinstance(data, list)
+def test_filtered_data():
+    assert filtered_data({}) == []
+    assert filtered_data("123434567") == []
+    assert filtered_data([{"state": "EXECUTED"}, {"state": "CANCELED"}]) == [{'state': 'EXECUTED'}]
+
+def test_get_last_values(test_data):
+    data = get_last_values(test_data, 3)
+    assert [x['date'] for x in data] == ['2019-07-03T18:35:29.512364', '2019-04-04T23:20:05.206878',
+                                         '2019-03-23T01:09:46.296404']
+
 
 def test_get_date():
-    assert get_date("2018-06-30T02:08:58.425572") == 3
-    2018
+    data = "2019-08-26T10:50:58.294041"
+    assert get_date(data) == "26.08.2019"
+
+def test_get_from_or_to():
+    data = "Maestro 1596837868705199"
+    assert get_from_or_to(data) == 'Maestro **9683'
+    data = "Счет 64686473678894779589"
+    assert get_from_or_to(data) == 'Счет 6468 64** **** 9589'
+
+def test_get_list_of_operations(test_data):
+    assert get_list_of_operations(test_data) == ['03.07.2019 Перевод организации '
+                                                 '\nMasterCard **5830 Счет 3538 30** **** 5560 '
+                                                 '\n8221.37 USD', '30.06.2018 Перевод организации '
+                                                                  '\nСчет 7510 68** **** 6952 Счет 1177 66** **** 6702 '
+                                                                  '\n9824.07 USD', '23.03.2018 Открытие вклада '
+                                                                                   '\n Счет 4142 15** **** 2431 '
+                                                                                   '\n48223.05 руб.',
+                                                 '04.04.2019 Перевод со счета на счет '
+                                                 '\nСчет 1970 86** **** 8542 Счет 7565 16** **** 4188 '
+                                                 '\n79114.93 USD', '23.03.2019 Перевод со счета на счет '
+                                                                   '\nСчет 4481 22** **** 4719 '
+                                                                   'Счет 7448 96** **** 1160 '
+                                                                   '\n43318.34 руб.']
--- utils/utils.py
+++ utils/utils.py
@@ -2,31 +2,34 @@ import json
 from datetime import date
 
 
-
-
 def unpacking_data():
+    """получает данные из файла json"""
     with open("operations.json", "r", encoding="utf-8") as file:
         data = json.load(file)
     return data
 
 
 def filtered_data(data):
+    """фильтрует полученые данные, (оставляет только выполненые транзакции)"""
     data = [x for x in data if "state" in x and x["state"] == "EXECUTED"]
     return data
 
 
 def get_last_values(data, count_last_values):
+    """отсортировывает последние(5) значений по ключам"""
     data = sorted(data, key=lambda x: x["date"], reverse=True)
     data = data[:count_last_values]
     return data
 
 
 def get_date(data):
+    """конвентирует дату"""
     data = date.fromisoformat(data[:10])
     return data.strftime("%d.%m.%Y")
 
 
 def get_from_or_to(data):
+    """конвертирует номер счета или карты"""
     data = data.split()
     if len(''.join(data[-1])) == 20:
         return f"{''.join(data[0:-1])} {data[-1][0:4]} {data[-1][4:6]}** **** {data[-1][-4:]}"
@@ -36,6 +39,8 @@ def get_from_or_to(data):
 
 
 def get_list_of_operations(data):
+    """все полученные через цикл данные по ключам переносит в новый список в необходимом порядке,
+    если в списке нет номера карты пропускаем"""
     list = []
     for row in data:
         date_of_operation = get_date(row["date"])
@@ -48,5 +53,5 @@ def get_list_of_operations(data):
         operation_amount = row["operationAmount"]["amount"]
         operation_cyrrency = row["operationAmount"]["currency"]["name"]
         list.append(f"{date_of_operation} {description} \n{open_from} "
-                              f"{to} \n{operation_amount} {operation_cyrrency}")
+                    f"{to} \n{operation_amount} {operation_cyrrency}")
     return list
